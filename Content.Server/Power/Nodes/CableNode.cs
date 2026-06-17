745fe542fb1fd9d7bdf4724e1b8b34640873f384
NeBorsh <Borisenkoff14@gmail.com>
2026-06-17 10:28:58 +0300

    init

--- Content.Server/Power/Nodes/CableNode.cs
+++ Content.Server/Power/Nodes/CableNode.cs
@@ -5,7 +5,7 @@ using Robust.Shared.Map.Components;
 namespace Content.Server.Power.Nodes
 {
     [DataDefinition]
-    public sealed partial class CableNode : Node
+    public partial class CableNode : Node
     {
         public override IEnumerable<Node> GetReachableNodes(
             Entity<TransformComponent> xform,
